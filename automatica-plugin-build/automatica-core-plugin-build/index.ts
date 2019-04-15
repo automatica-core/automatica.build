116806616ae4b8131412fa1995d1fead736b3802
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-04-15 08:27:18 +0200

    updated build tasks

--- automatica-plugin-build/automatica-core-plugin-build/index.ts
+++ automatica-plugin-build/automatica-core-plugin-build/index.ts
@@ -1,9 +1,21 @@
 import tl = require('azure-pipelines-task-lib/task');
-import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
+import process = require('process')
+var fs = require('fs');
 
 async function run() {
     try {
         const publish = tl.getBoolInput("publish", true);
+        const dockerize = tl.getBoolInput("dockerize", true);
+
+        let docker_user = "";
+        let docker_pw = "";
+        if(dockerize) {
+            docker_user = tl.getInput("docker_user", true);
+            docker_pw = tl.getInput("docker_password", true);
+            
+        }
+
+        const pluginName = tl.getInput("plugin_name", true);
         const manifestPath = tl.getInput("manifest_path", true);
         const version = tl.getInput("version", true);
         let apiKey: string = "";
@@ -26,7 +38,31 @@ async function run() {
             tl.setResult(tl.TaskResult.Failed, "Pack command failed");
             return;
         }
-        
+
+        if (dockerize) {
+            const dockerTag = `automaticacore/plugin-${pluginName}:${version}-${process.arch}`;
+            //  await run_cmd("cp", [outDir+"/*.acpkg", outDir+"plugin.acpkg"]);
+            
+
+            // fs.copyFileSync(`${outDir}/*.acpkg`, `${outDir}/plugin.acpkg`);
+
+            await docker_cli(["login", "-u", docker_user, "-p", docker_pw]);
+
+            var files = fs.readdirSync(outDir);
+
+            for (const f in files) {
+                console.log("files...", files[f]);
+                if (files[f].endsWith(".acpkg")) {
+                    console.log("copy from ", `${outDir}/${files[f]}`, `${tl.cwd()}/plugin.acpkg`);
+                    fs.copyFileSync(`${outDir}/${files[f]}`, `${tl.cwd()}/plugin.acpkg`);
+                    break;
+                }
+            }
+
+            await docker_cli(["build", "-f", "Dockerfile", "-t", dockerTag, "."]);
+            await docker_cli(["push", dockerTag]);
+        }
+
         if (publish) {
             const deployResult = await automatica_cli(["DeployPlugin", "-F", outDir + "/", "-A", apiKey, "-C", cloudUrl]);
 
@@ -43,10 +79,18 @@ async function run() {
 }
 
 async function automatica_cli(params: string[]): Promise<number> {
+    return run_cmd('automatica-cli', params);
+}
+
+async function docker_cli(params: string[]): Promise<number> {
+    return run_cmd("docker", params);
+}
+
+async function run_cmd(cmd: string, params: string[]) {
     return new Promise<number>((resolve) => {
         var spawn = require('child_process').spawn;
-        var prc = spawn('automatica-cli', params);
-        console.log("spawning automatica-cli with params", params);
+        var prc = spawn(cmd, params);
+        console.log("spawning ", cmd, "with params", params);
 
         //noinspection JSUnresolvedFunction
         prc.stdout.setEncoding('utf8');
